6f65a4ea99604ba80be838095d8d2795a7c3fbee
iDanyyy <danu.chiperi@gmail.com>
2025-03-26 01:02:24 +0200

    Page 2

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
-import { pdf } from '@react-pdf/renderer';
-import WebToPdf from '../src/pages/WebToPdf';
-import './App.css';
+import React, { useState } from "react";
+import { pdf } from "@react-pdf/renderer";
+import WebToPdf from "../src/pages/WebToPdf";
+import "./App.css";
 
 function App() {
   const [DocsInfo, setDocsInfo] = useState({
-    facultate: 'FACULTATEA DE ȘTIINȚE REALE, ECONOMICE ȘI ALE MEDIULUI',
-    catedra: 'CATEDRA DE MATEMATICĂ ȘI INFORMATICĂ',
-    unitateCurs: 'Proiectarea paginilor Web',
-    specialitate: '0613.4 Informatică',
-    ciclu: 'I',
-    studii: 'superioare de licență',
-    invatamant: 'cu frecvență',
-    autor1: 'dr. conf. univ., Ghenadie CABAC',
-    autor2: 'asist. univ., Sergiu CHILAT',
-    locSiAn: 'BĂLȚI, 2025',
+    facultate: "FACULTATEA DE ȘTIINȚE REALE, ECONOMICE ȘI ALE MEDIULUI",
+    catedra: "CATEDRA DE MATEMATICĂ ȘI INFORMATICĂ",
+    unitateCurs: "Proiectarea paginilor Web",
+    specialitate: "0613.4 Informatică",
+    ciclu: "I",
+    studii: "superioare de licență",
+    invatamant: "cu frecvență",
+    autor1: "dr. conf. univ., Ghenadie CABAC",
+    autor2: "asist. univ., Sergiu CHILAT",
+    locSiAn: "BĂLȚI, 2025",
+    procesNumber: "nr. 123",
+    procesDate: "11.02.2025",
+    assistUniv: "asist. univ.",
+    nameTeacher: "Vitalie ŢÎCĂU",
+    nameFaculty: "Facultății de Ştiinţe Reale, Economice și ale Mediului",
+    nameDecan: "Ina CIOBANU",
   });
 
   const EditInformation = (key, value) => {
@@ -24,126 +30,276 @@ function App() {
   const GeneratePDF = async () => {
     const blob = await pdf(<WebToPdf data={DocsInfo} />).toBlob();
     const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
-    a.download = 'Curriculum.pdf';
+    a.download = "Curriculum.pdf";
     a.click();
   };
 
   return (
     <div className="pdf-preview-style">
-        <div className="title-container">
-            <p className="bold">MINISTERUL EDUCAȚIEI ȘI CERCETĂRII AL REPUBLICII MOLDOVA</p>
-            <p className="bold">UNIVERSITATEA DE STAT „ALECU RUSSO” DIN BĂLȚI</p>
-            <p
-                className="bold editable"
+      <div className="title-container">
+        <p className="bold">
+          MINISTERUL EDUCAȚIEI ȘI CERCETĂRII AL REPUBLICII MOLDOVA
+        </p>
+        <p className="bold">UNIVERSITATEA DE STAT „ALECU RUSSO” DIN BĂLȚI</p>
+        <p
+          className="bold editable"
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={(e) => EditInformation("facultate", e.target.innerText)}
+        >
+          {DocsInfo.facultate}
+        </p>
+        <p
+          className="bold editable"
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={(e) => EditInformation("catedra", e.target.innerText)}
+        >
+          {DocsInfo.catedra}
+        </p>
+      </div>
+      <div className="curiculum-container-center">
+        <h2 className="center bold">CURRICULUM</h2>
+        <p className="center">la unitatea de curs</p>
+        <h2
+          className="center bold editable"
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={(e) => EditInformation("unitateCurs", e.target.innerText)}
+        >
+          {DocsInfo.unitateCurs}
+        </h2>
+        <p className="center">
+          pentru studenții de la specialitatea{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("specialitate", e.target.innerText)}
+          >
+            {DocsInfo.specialitate}
+          </span>
+        </p>
+        <p className="center">
+          Ciclul{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("ciclu", e.target.innerText)}
+          >
+            {DocsInfo.ciclu}
+          </span>
+          , studii{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("studii", e.target.innerText)}
+          >
+            {DocsInfo.studii}
+          </span>
+          , învățământ{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("invatamant", e.target.innerText)}
+          >
+            {DocsInfo.invatamant}
+          </span>
+        </p>
+      </div>
+      <div className="authors">
+        <div className="authors-container">
+          <div className="authors-title">
+            <p className="bold">Autorii:</p>
+          </div>
+          <div className="authors-content">
+            <div className="author">
+              <p
+                className="editable"
                 contentEditable
                 suppressContentEditableWarning
-                onBlur={(e) => EditInformation('facultate', e.target.innerText)}
-            >
-                {DocsInfo.facultate}
-            </p>
-            <p
-                className="bold editable"
-                contentEditable
-                suppressContentEditableWarning
-                onBlur={(e) => EditInformation('catedra', e.target.innerText)}
-            >
-                {DocsInfo.catedra}
-            </p>
-        </div>
-        <div className="curiculum-container-center">
-            <h2 className="center bold">CURRICULUM</h2>
-            <p className="center">la unitatea de curs</p>
-            <h2
-                className="center bold editable"
-                contentEditable
-                suppressContentEditableWarning
-                onBlur={(e) => EditInformation('unitateCurs', e.target.innerText)}
-            >
-                {DocsInfo.unitateCurs}
-            </h2>
-            <p className="center">
-                pentru studenții de la specialitatea{' '}
-                <span
-                    className="editable underline"
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => EditInformation('specialitate', e.target.innerText)}
-                >
-          {DocsInfo.specialitate}
-        </span>
-            </p>
-            <p className="center">
-                Ciclul{' '}
-                <span
-                    className="editable underline"
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => EditInformation('ciclu', e.target.innerText)}
-                >
-          {DocsInfo.ciclu}
-        </span>
-                , studii{' '}
-                <span
-                    className="editable underline"
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => EditInformation('studii', e.target.innerText)}
-                >
-          {DocsInfo.studii}
-        </span>
-                , învățământ{' '}
-                <span
-                    className="editable underline"
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => EditInformation('invatamant', e.target.innerText)}
-                >
-          {DocsInfo.invatamant}
-        </span>
-            </p>
-        </div>
-        <div className="authors">
-            <div className="authors-container">
-                <div className="authors-title">
-                    <p className="bold">Autorii:</p>
-                </div>
-                <div className="authors-content">
-                    <div className="author">
-                        <p
-                            className="editable"
-                            contentEditable
-                            suppressContentEditableWarning
-                            onBlur={(e) => EditInformation('autor1', e.target.innerText)}
-                        >
-                            {DocsInfo.autor1}
-                        </p>
-                    </div>
-                    <div className="author">
-                        <p
-                            className="editable"
-                            contentEditable
-                            suppressContentEditableWarning
-                            onBlur={(e) => EditInformation('autor2', e.target.innerText)}
-                        >
-                            {DocsInfo.autor2}
-                        </p>
-                    </div>
-                </div>
+                onBlur={(e) => EditInformation("autor1", e.target.innerText)}
+              >
+                {DocsInfo.autor1}
+              </p>
             </div>
-        </div>
-        <div className="city-container">
-            <p
-                className="bold center-city editable"
+            <div className="author">
+              <p
+                className="editable"
                 contentEditable
                 suppressContentEditableWarning
-                onBlur={(e) => EditInformation('locSiAn', e.target.innerText)}
-            >
-                {DocsInfo.locSiAn}
-            </p>
+                onBlur={(e) => EditInformation("autor2", e.target.innerText)}
+              >
+                {DocsInfo.autor2}
+              </p>
+            </div>
+          </div>
         </div>
-        <button onClick={GeneratePDF}>Descarcă PDF</button>
+      </div>
+      <div className="city-container">
+        <p
+          className="bold center-city editable"
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={(e) => EditInformation("locSiAn", e.target.innerText)}
+        >
+          {DocsInfo.locSiAn}
+        </p>
+      </div>
+      {/* Page 2 */}
+      <hr className="page-break" />
+      <div className="title-container">
+        <p className="center">
+          Discutat şi aprobat în şedinţa Catedrei de matematică şi informatică
+        </p>
+        <p className="center">
+          Procesul-verbal{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("procesNumber", e.target.innerText)}
+          >
+            {DocsInfo.procesNumber}
+          </span>{" "}
+          din{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("procesDate", e.target.innerText)}
+          >
+            {DocsInfo.procesDate}
+          </span>
+        </p>
+        <p className="center">
+          Şeful Catedrei de matematică şi informatică ______________{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("assistUniv", e.target.innerText)}
+          >
+            {DocsInfo.assistUniv}
+          </span>{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameTeacher", e.target.innerText)}
+          >
+            {DocsInfo.nameTeacher}
+          </span>
+        </p>
+        <p className="center">
+          Analizat și recomandat în ședința Comisiei metodice a{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameFaculty", e.target.innerText)}
+          >
+            {DocsInfo.nameFaculty}
+          </span>
+        </p>
+        <p className="center">
+          Procesul-verbal{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("procesNumber", e.target.innerText)}
+          >
+            {DocsInfo.procesNumber}
+          </span>{" "}
+          din{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("procesDate", e.target.innerText)}
+          >
+            {DocsInfo.procesDate}
+          </span>
+        </p>
+        <p className="center">
+          Președintele Comisiei metodice a Consiliului{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameFaculty", e.target.innerText)}
+          >
+            {DocsInfo.nameFaculty}
+          </span>
+          ______________{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameTeacher", e.target.innerText)}
+          >
+            {DocsInfo.nameTeacher}
+          </span>
+        </p>
+        <p className="center">
+          Discutat şi aprobat în şedinţa Consiliului{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameFaculty", e.target.innerText)}
+          >
+            {DocsInfo.nameFaculty}
+          </span>
+        </p>
+        <p className="center">
+          Procesul-verbal{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("procesNumber", e.target.innerText)}
+          >
+            {DocsInfo.procesNumber}
+          </span>{" "}
+          din{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("procesDate", e.target.innerText)}
+          >
+            {DocsInfo.procesDate}
+          </span>
+        </p>
+        <p className="center">
+          Decanul{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameFaculty", e.target.innerText)}
+          >
+            {DocsInfo.nameFaculty}
+          </span>{" "}
+          ______________{" "}
+          <span
+            className="editable underline"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => EditInformation("nameDecan", e.target.innerText)}
+          >
+            {DocsInfo.nameDecan}
+          </span>
+        </p>
+      </div>
+      <button onClick={GeneratePDF}>Descarcă PDF</button>
     </div>
   );
 }
--- app/src/pages/WebToPdf.jsx
+++ app/src/pages/WebToPdf.jsx
@@ -1,79 +1,124 @@
-import React from 'react';
-import { Document, Page, Text, View, StyleSheet, Font } from '@react-pdf/renderer';
-import Times from '../fonts/times.ttf';
-import Timesbd from '../fonts/timesbd.ttf';
-import Timesi from '../fonts/timesi.ttf';
-import Timesbi from '../fonts/timesbi.ttf';
+import React from "react";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
+import Times from "../fonts/times.ttf";
+import Timesbd from "../fonts/timesbd.ttf";
+import Timesi from "../fonts/timesi.ttf";
+import Timesbi from "../fonts/timesbi.ttf";
 
 Font.register({
-    family: 'Times',
-    src: Times,
-    format: 'truetype'
+  family: "Times",
+  src: Times,
+  format: "truetype",
 });
 Font.register({
-    family: 'TimesBold',
-    src: Timesbd,
-    format: 'truetype'
+  family: "TimesBold",
+  src: Timesbd,
+  format: "truetype",
 });
 Font.register({
-    family: 'TimesItalic',
-    src: Timesi,
-    format: 'truetype'
+  family: "TimesItalic",
+  src: Timesi,
+  format: "truetype",
 });
 Font.register({
-    family: 'TimesBoldItalic',
-    src: Timesbi,
-    format: 'truetype'
+  family: "TimesBoldItalic",
+  src: Timesbi,
+  format: "truetype",
 });
 
 const styles = StyleSheet.create({
-    page: {
-        fontFamily: 'Times',
-        fontSize: 12,
-        padding: 40,
-        lineHeight: 1.5,
-        textAlign: 'justify',
-    },
-    title: {
-        fontFamily: 'TimesBold',
-        fontSize: 12,
-        textAlign: 'center',
-    },
+  page: {
+    fontFamily: "Times",
+    fontSize: 12,
+    padding: 40,
+    lineHeight: 1.5,
+    textAlign: "justify",
+  },
+  title: {
+    fontFamily: "TimesBold",
+    fontSize: 12,
+    textAlign: "center",
+  },
 });
 
 const PdfDocument = ({ data }) => (
-    <Document language="ro">
-        <Page style={styles.page} size="A4">
-            <View style={styles.title}>
-                <View style={{ marginVertical: 16 }} />
-                <Text>MINISTERUL EDUCAȚIEI ȘI CERCETĂRII AL REPUBLICII MOLDOVA</Text>
-                <Text>UNIVERSITATEA DE STAT „ALECU RUSSO” DIN BĂLȚI</Text>
-                <Text>{data.facultate}</Text>
-                <Text>{data.catedra}</Text>
-            </View>
-            <View style={{ marginVertical: 105 }} />
-            <Text style={{ ...styles.title, fontSize: 16 }}>Curriculum</Text>
-            <View style={{ marginVertical: 4 }} />
-            <Text style={{ textAlign: 'center' }}>la unitatea de curs</Text>
-            <Text style={{ ...styles.title, fontSize: 18 }}>{data.unitateCurs}</Text>
-            <View style={{ marginVertical: 6 }} />
-            <Text style={{ textAlign: 'center' }}>
-                pentru studenții de la specialitatea <Text>{data.specialitate}</Text>
-            </Text>
-            <Text style={{ textAlign: 'center' }}>
-                Ciclul <Text>{data.ciclu}</Text>, studii <Text>{data.studii}</Text>, învățământ <Text>{data.invatamant}</Text>
-            </Text>
-            <View style={{ marginVertical: 30 }} />
-            <View style={{ alignSelf: 'flex-end' }}>
-                <Text style={{ fontFamily: 'TimesBold' }}>Autori:</Text>
-                <View style={{ marginVertical: 22 }} />
-                <Text>{data.autor1}</Text>
-                <Text>{data.autor2}</Text>
-            </View>
-            <View style={{ marginVertical: 80 }} />
-            <Text style={styles.title}>{data.locSiAn}</Text>
-        </Page>
-    </Document>
+  <Document language="ro">
+    <Page style={styles.page} size="A4">
+      <View style={styles.title}>
+        <View style={{ marginVertical: 16 }} />
+        <Text>MINISTERUL EDUCAȚIEI ȘI CERCETĂRII AL REPUBLICII MOLDOVA</Text>
+        <Text>UNIVERSITATEA DE STAT „ALECU RUSSO” DIN BĂLȚI</Text>
+        <Text>{data.facultate}</Text>
+        <Text>{data.catedra}</Text>
+      </View>
+      <View style={{ marginVertical: 105 }} />
+      <Text style={{ ...styles.title, fontSize: 16 }}>Curriculum</Text>
+      <View style={{ marginVertical: 4 }} />
+      <Text style={{ textAlign: "center" }}>la unitatea de curs</Text>
+      <Text style={{ ...styles.title, fontSize: 18 }}>{data.unitateCurs}</Text>
+      <View style={{ marginVertical: 6 }} />
+      <Text style={{ textAlign: "center" }}>
+        pentru studenții de la specialitatea <Text>{data.specialitate}</Text>
+      </Text>
+      <Text style={{ textAlign: "center" }}>
+        Ciclul <Text>{data.ciclu}</Text>, studii <Text>{data.studii}</Text>,
+        învățământ <Text>{data.invatamant}</Text>
+      </Text>
+      <View style={{ marginVertical: 30 }} />
+      <View style={{ alignSelf: "flex-end" }}>
+        <Text style={{ fontFamily: "TimesBold" }}>Autori:</Text>
+        <View style={{ marginVertical: 22 }} />
+        <Text>{data.autor1}</Text>
+        <Text>{data.autor2}</Text>
+      </View>
+      <View style={{ marginVertical: 80 }} />
+      <Text style={styles.title}>{data.locSiAn}</Text>
+    </Page>
+    <Page style={styles.page} size="A4">
+      <View style={{ marginVertical: 20 }} />
+      <Text style={{ textAlign: "center" }}>
+        Discutat şi aprobat în şedinţa Catedrei de matematică şi informatică
+      </Text>
+      <Text style={{ textAlign: "center" }}>
+        Procesul-verbal {data.procesNumber} din {data.procesDate}
+      </Text>
+      <View style={{ marginVertical: 10 }} />
+      <Text style={{ textAlign: "center" }}>
+        Șeful Catedrei de matematică şi informatică ______________{" "}
+        {data.assistUniv} {data.nameTeacher}
+      </Text>
+      <View style={{ marginVertical: 20 }} />
+      <Text style={{ textAlign: "center" }}>
+        Analizat și recomandat în ședința Comisiei metodice a {data.nameFaculty}
+      </Text>
+      <Text style={{ textAlign: "center" }}>
+        Procesul-verbal {data.procesNumber} din {data.procesDate}
+      </Text>
+      <View style={{ marginVertical: 10 }} />
+      <Text style={{ textAlign: "center" }}>
+        Președintele Comisiei metodice a Consiliului {data.nameFaculty}{" "}
+        ______________ {data.nameTeacher}
+      </Text>
+      <View style={{ marginVertical: 20 }} />
+      <Text style={{ textAlign: "center" }}>
+        Discutat şi aprobat în şedinţa Consiliului {data.nameFaculty}
+      </Text>
+      <Text style={{ textAlign: "center" }}>
+        Procesul-verbal {data.procesNumber} din {data.procesDate}
+      </Text>
+      <View style={{ marginVertical: 10 }} />
+      <Text style={{ textAlign: "center" }}>
+        Decanul {data.nameFaculty} ______________ {data.nameDecan}
+      </Text>
+    </Page>
+  </Document>
 );
 
 export default PdfDocument;
